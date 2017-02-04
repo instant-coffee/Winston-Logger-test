c626001eb397d09a0c2a4429f2dda24a0498f6de
Lachlan MacKenzie <lachlanrm@gmail.com>
2017-02-03 18:01:17 -0800

    Timestamps

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 'use strict';
 
 const winston = require('winston');
+const fs = require('fs');
+const env = process.env.NODE_ENV || 'development';
+const logDir = 'log';
+
+// Create the log directory if it does not exist
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
+
+const tsFormat = () => (new Date()).toLocaleTimeString();
+
+const logger = new (winston.Logger)({
+  transports: [
+    // colorize the output to the console
+    new (winston.transports.Console)({ 
+    	timestamp: tsFormat,
+    	colorize: true,
+    })
+  ]
+});
 
 winston.level = 'debug';
+
 winston.info('Hello world');
 winston.debug('Debugging info');
